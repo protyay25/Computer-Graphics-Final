c14bb50be7b3f68f8d9b37aa7d022b222dfd5392
Protyay Kumar Chanda <protyaykumaraiub25@gmail.com>
2026-01-21 06:45:21 +0600

    Protyay: Added enumeration, Global Veriables and Constants for Catching Game

--- openglportable/main.cpp
+++ openglportable/main.cpp
@@ -2,3 +2,17 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
+
+enum Scene { Menu, CatchGame, ShipAnimation };
+Scene currentScene = Menu;
+
+constexpr float FallingSpeed = 0.015f;
+constexpr float MoveSpeed = 0.12f;
+constexpr float PI = 3.1416f;
+
+float catcherX = 0.0f;
+float objX = 0.0f;
+float objY = 1.1f;
+int shapeType = 0;
+bool gameOver = false;
+int score = 0;
